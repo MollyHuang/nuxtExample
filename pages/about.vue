da1e1993f61b8ab25f61a8e36222cc9c782b68e3
Molly Huang <molly.h@gmail.com>
2022-02-28 22:50:37 +0800

    自訂表頭、metadata

--- pages/about.vue
+++ pages/about.vue
@@ -13,6 +13,24 @@ export default {
   middleware ({ store, route, redirect, params, query, req, res }) {
     // eslint-disable-next-line no-console
     console.log('Page middleware')
+  },
+  data () {
+    return {
+      title: 'Hello World!'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description'
+        }
+      ]
+    }
   }
 }
 </script>
