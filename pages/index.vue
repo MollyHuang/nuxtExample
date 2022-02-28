061f474d4679218bfcdc59bc67e946f04e4c555e
Molly Huang <molly.h@gmail.com>
2022-02-28 22:28:03 +0800

    validate (驗證參數) - 切換頁面後想要驗證一些 router 的參數的話，那麼就可以使用這個生命週期，但是請務必注意這個生命週期必須回傳 true or false。

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,17 @@ export default {
   nuxtServerInit (store, content) {
     // eslint-disable-next-line no-console
     console.log('nuxtServerInit')
+  },
+  middleware ({ store, route, redirect, params, query, req, res }) {
+    // eslint-disable-next-line no-console
+    console.log('Page middleware')
+  },
+  // 切換頁面後想要驗證一些 router 的參數的話適用 validate
+  // 務必注意 validate 這個生命週期必須回傳 true or false。
+  validate (content) {
+    // eslint-disable-next-line no-console
+    console.log('validate')
+    return true
   }
 }
 </script>
