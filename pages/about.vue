425eb8f1b99884c02d564877523a23e6e024ba9b
Molly Huang <molly.h@gmail.com>
2022-02-28 21:06:17 +0800

    添加 about page

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>
+      是 Ray 不是 Array
+      https://israynotarray.com
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
