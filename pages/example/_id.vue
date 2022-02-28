3346fe4533cbaeb65c463c09d0731d7d9f8519ab
Molly Huang <molly.h@gmail.com>
2022-02-28 21:35:19 +0800

    動態路由 pages/example/_id.vue

---
+++ pages/example/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <hr>
+    <p>動態路由 pages/example/_id.vue</p>
+    {{ $route.params.id }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExampleId',
+  created () {
+    // eslint-disable-next-line no-console
+    console.log(this.$route.params.id)
+  }
+}
+</script>
